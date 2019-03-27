27668ea51a367d0d4d062e9ab48157619b55b35e
Elnisky <leonardoelnisky@hotmail.com>
2019-03-27 13:50:58 -0300

    Add files via upload

---
+++ Kruskal/kruskal.h
@@ -0,0 +1,14 @@
+#include <stdlib.h>
+#include <stdio.h>
+#define N 6
+
+typedef struct aresta {
+	int v1, v2, custo;
+} aresta;
+
+void kruskal(int matriz[N][N]);
+void ordena(aresta *p, int t);
+void exibir_final(aresta *p, int t);
+
+
+
---
+++ Kruskal/main.c
@@ -0,0 +1,14 @@
+#include <stdlib.h>
+#include <stdio.h>
+#include "kruskal.h"
+#define N 6
+
+int main() {
+	int matriz_inicial[N][N] = {0,5,0,0,6,0,
+								5,0,7,0,13,9,
+								0,7,0,8,0,3,
+								0,0,8,0,9,15,
+								6,13,0,9,0,11,
+								0,9,3,15,11,0};
+	kruskal(matriz_inicial);
+}
